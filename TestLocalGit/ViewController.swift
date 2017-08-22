fcca3b4ff218a364b488c8d8d2ca4fb9b473cbbc
YongHwi <yonghwinam@smtown.com>
2017-08-22 12:25:51 +0900

    init

---
+++ TestLocalGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestLocalGit
+//
+//  Created by DEV_TEAM1_IOS on 22/08/2017.
+//  Copyright © 2017 S.M Entertainment. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ TestLocalGitTests/TestLocalGitTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestLocalGitTests.swift
+//  TestLocalGitTests
+//
+//  Created by DEV_TEAM1_IOS on 22/08/2017.
+//  Copyright © 2017 S.M Entertainment. All rights reserved.
+//
+
+import XCTest
+@testable import TestLocalGit
+
+class TestLocalGitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
